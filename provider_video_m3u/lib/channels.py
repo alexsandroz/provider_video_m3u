46841db8f2a15736743316ad58351fe5d7a366a2
alexsandroz <alexsandroz@gmail.com>
2025-05-01 03:54:23 -0300

    Implement m3u8proxy

--- provider_video_m3u/lib/channels.py
+++ provider_video_m3u/lib/channels.py
@@ -202,7 +202,8 @@ class Channels(PluginChannels):
         else:
             stream_url = ch_dict['json']['stream_url']
 
-        if self.config_obj.data[self.config_section]['player-stream_type'] == 'm3u8redirect':
+        if self.config_obj.data[self.config_section]['player-stream_type'] == 'm3u8redirect' or \
+                self.config_obj.data[self.config_section]['player-stream_type'] == 'm3u8proxy':
             return stream_url
 
         return self.get_best_stream(stream_url, 2, _channel_id)
